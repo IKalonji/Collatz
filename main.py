f128ae62aa50046f34c7a04ad72c306a99ebb346
IKalonji <IKalonji@student.wethinkcode.co.za>
2022-01-20 06:23:20 +0200

    Added solved map output

--- main.py
+++ main.py
@@ -2,18 +2,22 @@ from sys import argv
 import sys
 import threading
 
-number_find = int()
+'''Get commandline args'''
+argument = int()
 try:
-    number_find = int(argv[1])
+    argument = int(argv[1])
 except (ValueError, TypeError) as error:
-    print(error, "Cannot convert to an Int")
+    print(error, "Cannot convert arg to int")
     sys.exit()
 
-generated_list_to_solve = [i for i in range(1,number_find+1)]
-list_on_number_state = [0 for i in range(number_find)]
+'''Generated and state list setup'''
+generated_list_to_solve = [i for i in range(1,argument+1)]
+list_on_number_state = [0 for i in range(argument)]
+solved_map = {}
 
 def collatz_function(number, index):
-    print(f"Thread working on number {number} index {index}")
+    '''Collatz Conjecture Method'''
+    print(f"Thread working on number at index {index}")
     steps = 0
     while number != 1:
         if number % 2 == 0:
@@ -23,13 +27,10 @@ def collatz_function(number, index):
             number += 1
         steps += 1
     list_on_number_state[index] = steps
-    print(f"Thread ended working on number {number} index {index}")
-
-
+    solved_map[index+1] = str(steps) + " steps"
+    print(f"Thread ended working on number at index {index}")
 
 if __name__ == "__main__":
-    print("Generated list to solve: ", generated_list_to_solve)
-    print("State list to solve: ", list_on_number_state)
     while 0 in list_on_number_state[1:]:
         index_to_solve = list_on_number_state.index(0)
         list_on_number_state[index_to_solve] = -1
@@ -38,7 +39,6 @@ if __name__ == "__main__":
         thread.start()
     else:
         print("work done")
-        print(generated_list_to_solve)
-        print(list_on_number_state)
+        print(solved_map)
 
 
