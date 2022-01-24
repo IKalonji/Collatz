350f0728764ad0d9e0a55903e78695b7424810e2
IKalonji <IKalonji@student.wethinkcode.co.za>
2022-01-24 17:55:02 +0200

    added floor division

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def collatz_function(number):
     steps = 0
     while number != 1:
         if number % 2 == 0:
-            number /= 2
+            number //= 2
         else:
             number *= 3
             number += 1
